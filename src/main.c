c422dd32e38f62d785270579bd8e3c95a003dffa
Pedro Portales <pedroportales@riseup.net>
2023-12-13 19:19:28 +0000

    createTable Emprestimos

--- src/main.c
+++ src/main.c
@@ -12,14 +12,14 @@ int sqlite3_retorno(void *NotUsed, int argc, char **argv, char **coluna)
     return 0;
 }
 
-int createTable()
+int createUsuarios()
 {
     sqlite3 * db = 0;
     char * mensagem_erro = 0;
 
     printf("versao: %s\n", sqlite3_libversion());
 
-    int rc = sqlite3_open("database.db", &db);
+    int rc = sqlite3_open("database.sqlite", &db);
 
     if (rc != SQLITE_OK) 
     {
@@ -30,21 +30,69 @@ int createTable()
 
     // criando uma tabela dentro do arquivo
 
-    // Tabela usuarios
-    char create[] = "CREATE TABLE IF NOT EXISTS usuarios ("
+    // Tabela usuario    
+    char *create = "CREATE TABLE usuarios( "
                     "id INTEGER PRIMARY KEY AUTOINCREMENT,"
-                    "username TEXT UNIQUE NOT NULL"
+                    "usuario TEXT NOT NULL,"
                     "password TEXT NOT NULL);";
 
     rc = sqlite3_exec(db, create, sqlite3_retorno, 0, &mensagem_erro);
-
 
     sqlite3_close(db);
     return 0;
 }
 
+int createEmprestimos()
+{
+    sqlite3 * db = 0;
+    char * mensagem_erro = 0;
+
+    int rc = sqlite3_open("database.sqlite", &db);
+
+    if (rc != SQLITE_OK) 
+    {
+        printf("ERRO ao abrir: %s\n", sqlite3_errmsg(db));
+        sqlite3_close(db);
+        return 1;
+    }
+
+    char *create = "CREATE TABLE emprestimo( "
+                    "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                    "usuario_id INTEGER,"
+                    "valor REAL NOT NULL,"
+                    "taxa_juros REAL NOT NULL,"
+                    "meses INTEGER NOT NULL,"
+                    "FOREIGN KEY (usuario_id) REFERENCES usuarios (id))";
+
+    rc = sqlite3_exec(db, create, sqlite3_retorno, 0, &mensagem_erro);
+
+    sqlite3_close(db);
+    return 0;
+
+}  
+
+int inserirUsuario(char *username, char *password)
+{
+    sqlite3 * db = 0;
+    char * mensagem_erro = 0;
+
+    int rc = sqlite3_open("database.sqlite", &db);
+
+    if (rc != SQLITE_OK) 
+    {
+        printf("ERRO ao abrir: %s\n", sqlite3_errmsg(db));
+        sqlite3_close(db);
+        return 1;
+    }
+
+    char sql[100];
+    sprintf(sql, "INSERT INTO usuarios (username, password) VALUES");
+}
+
 int main(void) 
 {
-    createTable();
+    createUsuarios();
+    createEmprestimos();
+    inserirUsuario("usuario_teste", "senha123");
 }
  
