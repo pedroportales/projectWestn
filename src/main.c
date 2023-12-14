a7ec8c5fe0ff31e4630738f1f0b7bd9e8703b228
Pedro Portales <pedroportales@riseup.net>
2023-12-13 22:28:03 -0300

    insert db

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include "sqlite3.h"
 
 int sqlite3_retorno(void *NotUsed, int argc, char **argv, char **coluna) 
@@ -33,11 +34,12 @@ int createUsuarios()
     // Tabela usuario    
     char *create = "CREATE TABLE IF NOT EXISTS usuarios( "
                     "id INTEGER PRIMARY KEY AUTOINCREMENT,"
-                    "usuario UNIQUE TEXT NOT NULL,"
+                    "username TEXT UNIQUE NOT NULL,"
                     "password TEXT NOT NULL);";
 
     rc = sqlite3_exec(db, create, sqlite3_retorno, 0, &mensagem_erro);
 
+    sqlite3_free(mensagem_erro);
     sqlite3_close(db);
     return 0;
 }
@@ -56,7 +58,7 @@ int createEmprestimos()
         return 1;
     }
 
-    char *create = "CREATE TABLE emprestimo( "
+    char *create = "CREATE TABLE IF NOT EXISTS emprestimo( "
                     "id INTEGER PRIMARY KEY AUTOINCREMENT,"
                     "usuario_id INTEGER,"
                     "valor REAL NOT NULL,"
@@ -66,6 +68,7 @@ int createEmprestimos()
 
     rc = sqlite3_exec(db, create, sqlite3_retorno, 0, &mensagem_erro);
 
+    sqlite3_free(mensagem_erro);
     sqlite3_close(db);
     return 0;
 
@@ -86,7 +89,37 @@ int inserirUsuario(char *username, char *password)
     }
 
     char sql[100];
-    sprintf(sql, "INSERT INTO usuarios (username, password) VALUES");
+    sprintf(sql, "INSERT INTO usuarios (username, password) VALUES (\"%s\", \"%s\")", username, password);
+
+    rc = sqlite3_exec(db, sql, sqlite3_retorno, 0, &mensagem_erro);
+
+    sqlite3_free(mensagem_erro);
+    sqlite3_close(db);
+
+    return 0;
+}
+
+int inserirEmprestimo(int usuario_id, float valor, float taxa_juros, int meses)
+{
+    sqlite3 * db = 0;
+    char * mensagem_erro = 0;
+
+    int rc = sqlite3_open("database.sqlite", &db);
+
+    if (rc != SQLITE_OK) 
+    {
+        printf("ERRO ao abrir: %s\n", sqlite3_errmsg(db));
+        sqlite3_close(db);
+        return 1;
+    }
+
+    char sql[100];
+    sprintf(sql, "INSERT INTO emprestimo (usuario_id, valor, taxa_juros, meses) VALUES (%d, %f, %f, %d);", usuario_id, valor, taxa_juros, meses);
+
+    rc = sqlite3_exec(db, sql, sqlite3_retorno, 0, &mensagem_erro);
+
+    sqlite3_free(mensagem_erro);
+    sqlite3_close(db);
 
     return 0;
 }
@@ -96,5 +129,6 @@ int main(void)
     createUsuarios();
     createEmprestimos();
     inserirUsuario("usuario_teste", "senha123");
+    inserirEmprestimo(1, 1000.0, 12.75, 12);
 }
  
